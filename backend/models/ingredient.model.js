2285c0a249e96b66ee91f4bd49c3927934985795
Michael Gift <simiyugift@outlook.com>
2024-09-11 21:50:17 +0300

    Ingredient model

---
+++ backend/models/ingredient.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const IngredientSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter ingredient name"],
+        },
+
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Ingredient = mongoose.model('Ingredient', IngredientSchema);
+module.exports = Ingredient;
