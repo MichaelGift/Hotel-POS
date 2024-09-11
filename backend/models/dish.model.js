21c98ab4dad9ffbc0596883f23c939da096e830f
Michael Gift <simiyugift@outlook.com>
2024-09-12 01:27:17 +0300

    Create dish model

---
+++ backend/models/dish.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const dishSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter dish name"],
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+
+        ingredients: [
+            {
+                ingredient: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Ingredient',
+                    required: true,
+                },
+                quantityRequired: {
+                    type: Number,
+                    required: true,
+                    default: 0,
+                }
+            },
+        ]
+    }
+);
+
+const Dish = mongoose.model('Dish', dishSchema);
+module.exports = Dish;
