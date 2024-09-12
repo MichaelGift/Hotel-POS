ba635bdb8a7b81ee5e563062f2c59c7c83c226b7
Michael Gift <simiyugift@outlook.com>
2024-09-13 00:50:07 +0300

    Create Inventory page

---
+++ client/page/Inventory.tsx
@@ -0,0 +1,10 @@
+import {useState} from "react";
+
+const Inventory = () => {
+    const [ingredient, fetchIngredients] = useState<Ingredients[]>([])
+
+
+
+    return (<></>)
+}
+export default Inventory
