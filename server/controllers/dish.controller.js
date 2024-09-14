86ce0f4dafb38798ea6d7099c425a0e2e5e5ee37
Michael Gift <simiyugift@outlook.com>
2024-09-14 21:08:35 +0300

    Populate ingredient array.

--- server/controllers/dish.controller.js
+++ server/controllers/dish.controller.js
@@ -11,7 +11,7 @@ const addDish = async (req, res) => {
 
 const getDishes = async (req, res) => {
     try {
-        const dishes = await Dish.find({})
+        const dishes = await Dish.find({}).populate('ingredients.ingredient')
         res.status(200).json(dishes)
     } catch (error) {
         res.status(500).json({message: error.message})
