edf9f4ba1534fb011171e2c9d0f5ec35ec419567
Michael Gift <simiyugift@outlook.com>
2024-09-17 23:31:19 +0300

    Create index for exporting all components

---
+++ client/src/components/index.tsx
@@ -0,0 +1,9 @@
+import SideNavButton from "./SideNavButton.tsx";
+import MenuDish from "./MenuDish.tsx";
+import OrderItem from "./OrderItem.tsx";
+
+export {
+    SideNavButton,
+    MenuDish,
+    OrderItem
+}
