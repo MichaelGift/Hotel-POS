46243cc4e1d826d3ebc1c42438215c4d07dcff1d
Michael Gift <simiyugift@outlook.com>
2024-09-18 02:12:27 +0300

    Export IngredientCard via component

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,9 +1,11 @@
 import SideNavButton from "./SideNavButton.tsx";
 import MenuDish from "./MenuDish.tsx";
 import OrderItem from "./OrderItem.tsx";
+import IngredientCard from "./IngredientCard.tsx";
 
 export {
     SideNavButton,
     MenuDish,
-    OrderItem
+    OrderItem,
+    IngredientCard
 }
