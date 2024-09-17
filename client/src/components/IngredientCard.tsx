35b127461b76f90d0a520b5c0c8e40c60e3439ec
Michael Gift <simiyugift@outlook.com>
2024-09-18 02:11:07 +0300

    Create Ingredient card

---
+++ client/src/components/IngredientCard.tsx
@@ -0,0 +1,12 @@
+const IngredientCard = ({name, price, quantity}) => (
+    <div className="col-md-3 p1">
+        <button className="btn text-light w-100 h-100 p-3 rounded"
+                style={{backgroundColor: '#2d2d2d'}}>
+            <h5 className='m-0 p-0'>{name}</h5>
+            <p className="text-secondary">Ksh {price}</p>
+            <p className="text-secondary">x{quantity}</p>
+        </button>
+    </div>
+)
+
+export default IngredientCard
