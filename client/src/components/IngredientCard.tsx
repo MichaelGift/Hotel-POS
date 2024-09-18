7c23d8300bd47f5e391429b65011faca22e2df56
Michael Gift <simiyugift@outlook.com>
2024-09-18 03:52:57 +0300

    Remove price for ingredients

--- client/src/components/IngredientCard.tsx
+++ client/src/components/IngredientCard.tsx
@@ -1,11 +1,10 @@
 const IngredientCard = ({name, price, quantity, onClick}) => (
-    <div className="col-md-3 p1">
+    <div className="col-md-3 p-1">
         <button className="btn text-light w-100 h-100 p-3 rounded"
                 style={{backgroundColor: '#2d2d2d'}}
                 onClick={onClick}>
             <h5 className='m-0 p-0'>{name}</h5>
-            <p className="text-secondary">Ksh {price}</p>
-            <p className="text-secondary">x{quantity}</p>
+            <p className="text-secondary m-0 p-0">x{quantity}</p>
         </button>
     </div>
 )
