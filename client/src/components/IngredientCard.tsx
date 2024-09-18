11fcc35bcb058341ee622e19229b661ca02a3212
Michael Gift <simiyugift@outlook.com>
2024-09-18 03:47:31 +0300

    Add clickability

--- client/src/components/IngredientCard.tsx
+++ client/src/components/IngredientCard.tsx
@@ -1,7 +1,8 @@
-const IngredientCard = ({name, price, quantity}) => (
+const IngredientCard = ({name, price, quantity, onClick}) => (
     <div className="col-md-3 p1">
         <button className="btn text-light w-100 h-100 p-3 rounded"
-                style={{backgroundColor: '#2d2d2d'}}>
+                style={{backgroundColor: '#2d2d2d'}}
+                onClick={onClick}>
             <h5 className='m-0 p-0'>{name}</h5>
             <p className="text-secondary">Ksh {price}</p>
             <p className="text-secondary">x{quantity}</p>
