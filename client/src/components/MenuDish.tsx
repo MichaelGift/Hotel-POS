9c7a2c8a12114e63cf64ae3924749050e08c0280
Michael Gift <simiyugift@outlook.com>
2024-09-19 16:23:25 +0300

    Add price and subprice for menu foods

--- client/src/components/MenuDish.tsx
+++ client/src/components/MenuDish.tsx
@@ -1,9 +1,13 @@
 const MenuDish = ({dish, onClick}) => (
     <div className="col-md-3 p-1">
-        <button className="btn text-light w-100 h-100 p-3 rounded"
-                style={{backgroundColor: '#2d2d2d'}}
-                onClick={onClick}
-        >{dish.name}</button>
+        <button
+            className="btn text-light w-100 h-100 p-3 rounded"
+            style={{backgroundColor: '#2d2d2d'}}
+            onClick={onClick}>
+
+            <h6 className={'m-0 p-0'}>{dish.name}</h6>
+            <p className={'text-secondary m-0 p-0'}>Ksh {dish.price}</p>
+        </button>
     </div>
 )
 
