e9af575ccfa1a51ff4301274e987bc04dad290bb
Michael Gift <simiyugift@outlook.com>
2024-09-20 03:23:29 +0300

    Add footer note.
    >> Getting fancy with the design

--- client/src/page/POS.tsx
+++ client/src/page/POS.tsx
@@ -1,20 +1,56 @@
-import {SideNavButton} from "../components";
-
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import {useState} from "react";
 
 const PoS = () => {
+    const [selection, setSelection] = useState('menu');
+
     return (
         <>
             <div className="container text-light mh-100 mw-100" style={{backgroundColor: '#111315'}}>
                 <div className="row">
                     <div className="col-2 d-flex flex-column shadow-lg nav" style={{height: '100vh'}}>
                         <div className="mb-5 mt-2">
-                            <h2>Hotel POS</h2></div>
-                        <SideNavButton text="Menu" route='menu'/>
-                        <SideNavButton text='Orders' route='order'/>
-                        <SideNavButton text="Tables" route='tables'/>
-                        <SideNavButton text="Dishes" route='dishes'/>
-                        <SideNavButton text="Inventory" route='inventory'/>
+                            <h2>Hotel POS</h2>
+                        </div>
+                        <ul className={'nav nav-pills flex-column'}>
+                            <li className={'nav-item'}>
+                                <Link to={'menu'}
+                                      className={`nav-link btn text-light mb-1 p-2 ${selection === 'menu' ? 'active' : ''} rounded w-100`}
+                                      onClick={() => setSelection('menu')}>Menu</Link>
+                            </li>
+                            <li className={'nav-item'}>
+                                <Link to={'order'}
+                                      className={`nav-link btn text-light mb-1 p-2 ${selection === 'orders' ? 'active' : ''} rounded w-100`}
+                                      onClick={() => setSelection('orders')}>Orders</Link>
+                            </li>
+                            <li className={'nav-item'}>
+                                <Link to={'tables'}
+                                      className={`nav-link btn text-light mb-1 p-2 ${selection === 'tables' ? 'active' : ''} rounded w-100`}
+                                      onClick={() => setSelection('tables')}>Tables</Link>
+                            </li>
+                            <li className={'nav-item'}>
+                                <Link to={'dishes'}
+                                      className={`nav-link btn text-light mb-1 p-2 ${selection === 'dishes' ? 'active' : ''} rounded w-100`}
+                                      onClick={() => setSelection('dishes')}>Dishes</Link>
+                            </li>
+                            <li className={'nav-item'}>
+                                <Link to={'inventory'}
+                                      className={`nav-link btn text-light mb-1 p-2 ${selection === 'inventory' ? 'active' : ''} rounded w-100`}
+                                      onClick={() => setSelection('inventory')}>Inventory</Link>
+                            </li>
+                        </ul>
+
+                        <div className={'mt-auto w-100 d-flex justify-content-center'}>
+
+                            <figure className="text-center">
+                                <blockquote className="blockquote">
+                                    <p className={'small text-muted'}>Do hard things.</p>
+                                </blockquote>
+                                <figcaption className="blockquote-footer">
+                                    With <span style={{color: "red"}}>♥</span> from Antares
+                                </figcaption>
+                            </figure>
+                        </div>
                     </div>
                     <main className="col-10" style={{height: '100vh'}}>
                         <Outlet/>
